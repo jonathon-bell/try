6c2bc2e857a731cfe27d74881e743c4bd52b31b0
Jonathon Bell <jonathon.a.bell@gmail.com>
2017-03-28 08:43:56 -0400

    .

--- src/main/scala/com/wolery/owl/TransportController.scala
+++ src/main/scala/com/wolery/owl/TransportController.scala
@@ -16,91 +16,118 @@ package com.wolery.owl
 
 //****************************************************************************
 
-import com.wolery.owl.core.Bool
-import com.wolery.owl.midi.messages._
+import com.wolery.owl.core._
+import com.wolery.owl.midi.messages.HARMONY
+import com.wolery.owl.midi.messages.MetaMessageEx
+import com.wolery.owl.midi.messages.TEMPO
+import com.wolery.owl.midi.messages.TIME
+import com.wolery.owl.utils.implicits.asEventHandler
 import com.wolery.owl.utils.implicits.asRunnable
 
+import javafx.animation.KeyFrame
+import javafx.animation.Timeline
 import javafx.application.Platform.{ runLater ⇒ defer }
 import javafx.css.PseudoClass.getPseudoClass
+import javafx.event.ActionEvent
 import javafx.fxml.{ FXML ⇒ fx }
 import javafx.scene.control.Label
 import javafx.scene.input.MouseEvent
+import javafx.util.Duration.millis
 import javax.sound.midi.MetaEventListener
 import javax.sound.midi.MetaMessage
+import javax.sound.midi.Sequencer
 
 //****************************************************************************
 
 class TransportController extends MetaEventListener
 {
-  @fx var prev:  Label = _
-  @fx var next:  Label = _
-  @fx var loop:  Label = _
-  @fx var rset:  Label = _
-  @fx var rwnd:  Label = _
-  @fx var advn:  Label = _
-  @fx var stop:  Label = _
-  @fx var play:  Label = _
+  @fx var m_prev:  Label = _
+  @fx var m_next:  Label = _
+  @fx var m_loop:  Label = _
+  @fx var m_rset:  Label = _
+  @fx var m_rwnd:  Label = _
+  @fx var m_advn:  Label = _
+  @fx var m_stop:  Label = _
+  @fx var m_play:  Label = _
 
-  @fx var secs:  Label = _
-  @fx var bars:  Label = _
-  @fx var left:  Label = _
-  @fx var right: Label = _
-  @fx var tempo: Label = _
+  @fx var m_harm:  Label = _
+  @fx var m_bars:  Label = _
+  @fx var m_left:  Label = _
+  @fx var m_right: Label = _
+  @fx var m_tempo: Label = _
+
+      val m_seq: Sequencer = owl.sequencer
+      val m_tmr: Timeline = newTimer()
+      var m_meter: (ℕ,ℕ) = (1,1)
 
   def initialize(): Unit =
   {
-    prev.setText("\uf048")
-    next.setText("\uf051")
-    loop.setText("\uf01e")
-    rset.setText("\uf049")
-    rwnd.setText("\uf04a")
-    advn.setText("\uf04e")
-    stop.setText("\uf04d")
-    play.setText("\uf04b")
+    m_prev.setText("\uf048")
+    m_next.setText("\uf051")
+    m_loop.setText("\uf01e")
+    m_rset.setText("\uf049")
+    m_rwnd.setText("\uf04a")
+    m_advn.setText("\uf04e")
+    m_stop.setText("\uf04d")
+    m_play.setText("\uf04b")
 
-    tempo.setText("1.00")
+    m_tempo.setText("1.00")
 
     onStateChange()
   }
 
   def meta(message: MetaMessage): Unit = message.getType match
   {
-    case TEMPO ⇒ defer(onTempoChange(message.tempo))
-    case _     ⇒
+    case TEMPO   ⇒ defer(onTempoChange(message.tempo))
+    case TIME    ⇒ defer(onMeterChange(message.time))
+    case HARMONY ⇒ defer(onScaleChange(message.scale))
+    case _       ⇒
   }
 
   def onTempoChange(bpm: ℝ) =
   {
-    tempo.setText(f"$bpm%2.2f")
+    m_tempo.setText(f"$bpm%2.2f")
+  }
+
+  def onMeterChange(meter: (ℕ,ℕ)): Unit =
+  {
+    m_meter = meter
+  }
+
+  def onScaleChange(scale: Scale): Unit =
+  {
+    m_harm.setText(scale.toString)
   }
 
   def onReset(e: MouseEvent): Unit =
   {
-    owl.sequencer.setTickPosition(0)
+    m_seq.setTickPosition(0)
   }
 
   def onStateChange(): Unit =
   {
-    stop.pseudoClassStateChanged(STOPPED,!isPlaying)
-    play.pseudoClassStateChanged(PLAYING, isPlaying)
-    loop.pseudoClassStateChanged(LOOPING, isLooping)
+    m_stop.pseudoClassStateChanged(STOPPED,!isPlaying)
+    m_play.pseudoClassStateChanged(PLAYING, isPlaying)
+    m_loop.pseudoClassStateChanged(LOOPING, isLooping)
   }
 
   def onStop(e: MouseEvent): Unit =
   {
-    owl.sequencer.stop()
+    m_seq.stop()
+    m_tmr.stop()
     onStateChange()
   }
 
   def onPlay(e: MouseEvent): Unit =
   {
-    owl.sequencer.start()
+    m_seq.start()
+    m_tmr.play()
     onStateChange()
   }
 
   def onLoop(e: MouseEvent): Unit =
   {
-    owl.sequencer.setLoopCount(if (isLooping) 0 else -1)
+    m_seq.setLoopCount(if (isLooping) 0 else -1)
     onStateChange()
   }
 
@@ -108,9 +135,26 @@ class TransportController extends MetaEventListener
   def onNext     (e: MouseEvent): Unit = println("next")
   def onRewind   (e: MouseEvent): Unit = println("rewind")
   def onAdvance  (e: MouseEvent): Unit = println("advance")
-
   def isLooping: Bool = owl.sequencer.getLoopCount != 0
   def isPlaying: Bool = owl.sequencer.isRunning
+
+  def onUpdate(): Unit =
+  {
+    assert(m_seq.getSequence.getDivisionType == 0) // PPQ
+
+    val tick = m_seq.getTickPosition
+    val bars = tick / m_meter._1
+    val beat = tick % m_meter._1
+
+    m_bars.setText(f"$bars%04d.$beat%02d.00")
+  }
+
+  def newTimer(): Timeline =
+  {
+    val t = new Timeline(new KeyFrame(millis(100),(a:ActionEvent) ⇒ onUpdate()))
+    t.setCycleCount(-1)
+    t
+  }
 
   val STOPPED = getPseudoClass("stopped")
   val PLAYING = getPseudoClass("playing")
