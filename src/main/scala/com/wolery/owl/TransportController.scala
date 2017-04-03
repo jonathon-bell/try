69910f68c8e7d35f0c4f7b3a6352497e244092ef
Jonathon Bell <jonathon.a.bell@gmail.com>
2017-04-03 03:25:49 -0400

    .

--- src/main/scala/com/wolery/owl/TransportController.scala
+++ src/main/scala/com/wolery/owl/TransportController.scala
@@ -16,35 +16,25 @@ package com.wolery.owl
 
 //****************************************************************************
 
-import com.wolery.owl.core._
-import com.wolery.owl.core.utilities._
-import com.wolery.owl.midi.messages._
-import com.wolery.owl.utils.implicits._
-import com.wolery.owl.utils.implicits.asEventHandler
-import com.wolery.owl.utils.implicits.asRunnable
+import Math.max
 
-import javafx.animation._
-import javafx.animation.KeyFrame
-import javafx.animation.Timeline
+import com.wolery.owl.core.{ Bool, Meter, Scale }
+import com.wolery.owl.core.utilities.between
+import com.wolery.owl.midi.messages.{ METER, MetaMessageEx, SCALE, TEMPO }
+import com.wolery.owl.utils.implicits.{ asChangeListener, asEventHandler, asRunnable }
+
+import javafx.animation.{ KeyFrame, Timeline }
 import javafx.application.Platform.{ runLater ⇒ defer }
+import javafx.beans.value.ObservableValue
 import javafx.css.PseudoClass.getPseudoClass
-import javafx.event._
 import javafx.event.ActionEvent
 import javafx.fxml.{ FXML ⇒ fx }
-import javafx.scene.control.Label
+import javafx.scene.control.{ Label, Spinner, SpinnerValueFactory }
+import javafx.scene.control.SpinnerValueFactory.DoubleSpinnerValueFactory
 import javafx.scene.input.MouseEvent
 import javafx.util.Duration.millis
-import javax.sound.midi.MetaEventListener
-import javax.sound.midi.MetaMessage
-import javax.sound.midi.Sequence._
-import javax.sound.midi.Sequencer
-
-import javafx.scene.control.Spinner
-import javafx.scene.control.SpinnerValueFactory
-import javafx.scene.control.SpinnerValueFactory.DoubleSpinnerValueFactory
-import javafx.beans.value.ChangeListener
-import javafx.beans.value.ObservableValue
-import javafx.util.StringConverter;
+import javafx.util.StringConverter
+import javax.sound.midi.{ MetaEventListener, MetaMessage, Sequencer }
 
 //****************************************************************************
 
@@ -61,7 +51,7 @@ class TransportController extends MetaEventListener
   @fx var m_loop:  Pane  = _
   @fx var m_left:  Label = _
   @fx var m_right: Label = _
-  @fx var m_tempo: Spinner[ℝℝ] = _
+  @fx var m_tempo: Spinner[ℝ] = _
   @fx var m_meter: Label = _
   @fx var m_scale: Label = _
 
@@ -69,21 +59,9 @@ class TransportController extends MetaEventListener
       val m_tmr  : Timeline  = newTimer()
       var m_mtr  : Meter     = _
 
-  type ℝℝ    = java.lang.Double
-
   def initialize(): Unit =
   {
-    val f = (new DoubleSpinnerValueFactory(2,208,getEffectiveTempoInBPM))
-    val c = f.getConverter
-    f.setConverter(new StringConverter[ℝℝ]
-    {
-      def toString  (r: ℝℝ): String = f"$r%.2f"
-      def fromString(s: String): ℝℝ = c.fromString(s)
-    })
-
-    m_tempo.setValueFactory(f)
-    m_tempo.valueProperty.addListener(onTempoSpinChange _)
-
+    initTempoSpinner()
     onMeterChange(Meter(4,4))
     updateButtons()
   }
@@ -106,10 +84,10 @@ class TransportController extends MetaEventListener
   {
     val fac = m_seq.getTempoFactor
 
-//    m_tempo.getValueFactory.setValue(bpm * fac)
+    m_tempo.getValueFactory.setValue(bpm * fac)
   }
 
-  def onTempoSpinChange(ov: ObservableValue[_<: ℝℝ],was: ℝℝ,now: ℝℝ): Unit =
+  def onTempoSpinChange(ov: ObservableValue[_<: ℝ],was: ℝ,now: ℝ): Unit =
   {
     val was = m_seq.getTempoInBPM
     val fac = now / was
@@ -166,18 +144,18 @@ class TransportController extends MetaEventListener
     if (isPlaying)
     {
       val t = m_seq.getTickPosition
-      val d = Math.max(t - m_tap, 0)
+      val d = max(t - m_tap, 0)
       val s = if (me.isShiftDown)   2 else 1
       val c = if (me.isControlDown) 4 else 1
       val e = getEffectiveTempoInTPM.toFloat / (d *  s * c)
 
       if (between(e,2,208))
       {
-    val was = m_seq.getTempoInBPM
-    val fac = e / was
+        val was = m_seq.getTempoInBPM
+        val fac = e / was
 
-    m_seq.setTempoFactor(fac.toFloat)
-    m_tempo.getValueFactory.setValue(e)
+        m_seq.setTempoFactor(fac.toFloat)
+        m_tempo.getValueFactory.setValue(e)
       }
 
       m_tap = t
@@ -222,57 +200,26 @@ class TransportController extends MetaEventListener
     t
   }
 
+  def initTempoSpinner() =
+  {
+    val f = new DoubleSpinnerValueFactory(2,208,getEffectiveTempoInBPM,1)
+    {
+      val c = getConverter
+
+      setConverter(new StringConverter[java.lang.Double]
+      {
+        def toString  (r: java.lang.Double): String = f"$r%.2f"
+        def fromString(s: String): java.lang.Double = c.fromString(s)
+      })
+    }
+
+    m_tempo.setValueFactory(f.asInstanceOf[SpinnerValueFactory[ℝ]])
+    m_tempo.valueProperty.addListener(onTempoSpinChange _)
+  }
+
   val STOPPED = getPseudoClass("stopped")
   val PLAYING = getPseudoClass("playing")
   val LOOPING = getPseudoClass("looping")
 }
 
 //****************************************************************************
-/*
-class TempoSpinnerValueFactory(m_min: Double = 1,m_max: Double = 208,m_val: Double = 120,m_step: Double=1) extends SpinnerValueFactory[Double]
-{
-  setMin(m_min)
-  this.setMax(m_max)
-  this.setAmountToStepBy(m_step)
-  this.setConverter(new StringConverter[Double]
-  {
-      def toString(value:Double): String = f"$value%.2f"
-
-      def fromString(value:String):Double
-      {
-          try
-          {
-              // If the specified value is null or zero-length, return null
-              if (value == null) {
-                  return null;
-              }
-
-              value = value.trim();
-
-              if (value.length() < 1)
-              {
-                  return null;
-              }
-
-              // Perform the requested parsing
-              return df.parse(value).doubleValue();
-          }
-          catchsex)
-          {
-              throw new RuntimeException(ex);
-          }
-      }
-  });
-
-  valueProperty().addListener((o, oldValue, newValue) -> {
-
-      if (newValue < getMin()) {
-          setValue(getMin());
-      } else if (newValue > getMax()) {
-          setValue(getMax());
-      }
-  });
-
-  setValue(initialValue >= min && initialValue <= max ? initialValue : min);
-}
-*/
