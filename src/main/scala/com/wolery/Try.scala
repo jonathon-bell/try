f7296ace2eb1bd6ac4a572f58ffc2cae3b0a25ac
Jonathon Bell <jonathon.a.bell@gmail.com>
2017-10-20 16:40:19 -0400

    .

--- src/main/scala/com/wolery/Try.scala
+++ src/main/scala/com/wolery/Try.scala
@@ -8,40 +8,14 @@
 //*
 //*
 //*  Comments: This file uses a tab size of 2 spaces.
-//*
-//*
-//****************************************************************************
-
-package com.wolery
-import scala.tools.nsc.Settings
-import scala.tools.nsc.interpreter.IMain
-
-//****************************************************************************
+//*                                                                     0-0
+//*                                                                   (| v |)
+//**********************************************************************w*w***
 
 object Main
 {
   def main(args: Array[String]): Unit =
   {
-    println("Howdy")
-    val s = new Settings()
-    s.usejavacp.value = true
-    println(s.usejavacp.value)
-
-    val urls = this.getClass.getClassLoader match
-    {
-      case cl: java.net.URLClassLoader => cl.getURLs.toList
-    }
-
-    for (url <- urls)
-      println(url)
-    val i = new IMain(s)
-
-    s.classpath.value =
-    "/Users/jbell/.m2/repository/org/scala-lang/scala-library/2.12.2/scala-library-2.12.2.jar"
-    println(s.classpath.value)
-    i.bind("X","Int",78)
-
-    println("Done")
   }
 }
 
